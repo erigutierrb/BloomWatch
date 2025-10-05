9483476073d33ba51c8da92f49580959ac0d6046
Erika <erika.giselle.gb@hotmail.com>
2025-10-05 12:08:28 -0500

    simulation page

---
+++ mapdata.js
@@ -0,0 +1,447 @@
+var simplemaps_countrymap_mapdata={
+  main_settings: {
+    //General settings
+		width: "responsive", //or 'responsive'
+    background_color: "#FFFFFF",
+    background_transparent: "yes",
+    border_color: "#ffffff",
+    pop_ups: "detect",
+    
+		//State defaults
+		state_description: "Not Applicable",
+    state_color: "#88A4BC",
+    state_hover_color: "#3B729F",
+    state_url: "",
+    border_size: 1.5,
+    all_states_inactive: "no",
+    all_states_zoomable: "yes",
+    
+		//Location defaults
+		/*location_description: "Location description",
+    location_url: "",
+    location_color: "#FFF",
+    location_opacity: 0.8,
+    location_hover_opacity: 1,
+    location_size: 25,
+    location_type: "square",
+    location_image_source: "frog.png",
+    location_border_color: "#FFFFFF",
+    location_border: 2,
+    location_hover_border: 2.5,
+    all_locations_inactive: "no",
+    all_locations_hidden: "no", */
+    
+		//Label defaults
+		label_color: "#ffffff",
+    label_hover_color: "#ffffff",
+    label_size: 16,
+    label_font: "Arial",
+    label_display: "auto",
+    label_scale: "yes",
+    hide_labels: "no",
+    hide_eastern_labels: "no",
+   
+		//Zoom settings
+		zoom: "yes",
+    manual_zoom: "yes",
+    back_image: "no",
+    initial_back: "no",
+    initial_zoom: "-1",
+    initial_zoom_solo: "no",
+    region_opacity: 1,
+    region_hover_opacity: 0.6,
+    zoom_out_incrementally: "yes",
+    zoom_percentage: 0.99,
+    zoom_time: 0.5,
+    
+		//Popup settings
+		popup_color: "white",
+    popup_opacity: 0.9,
+    popup_shadow: 1,
+    popup_corners: 5,
+    popup_font: "12px/1.5 Verdana, Arial, Helvetica, sans-serif",
+    popup_nocss: "no",
+    
+		//Advanced settings
+		div: "map",
+    auto_load: "yes",
+    url_new_tab: "no",
+    images_directory: "default",
+    fade_time: 0.1,
+    link_text: "View Website"
+  },
+  state_specific: {
+    COAMA: {
+      name: "Amazonas",
+      description: "default",
+      color: "default",
+      hover_color: "default",
+      url: "default"
+    },
+    COANT: {
+      name: "Antioquia",
+      description: "☕ Café: 72%\n\n🍌 Plátano: 65%\n\nClick to view full detail.",
+      color: "#10B981",
+      hover_color: "default",
+      url: "default"
+    },
+    COARA: {
+      name: "Arauca",
+      description: "default",
+      color: "default",
+      hover_color: "default",
+      url: "default"
+    },
+    COATL: {
+      name: "Atlántico",
+      description: "default",
+      color: "default",
+      hover_color: "default",
+      url: "default"
+    },
+    COBOL: {
+      name: "Bolívar",
+      description: "default",
+      color: "default",
+      hover_color: "default",
+      url: "default"
+    },
+    COBOY: {
+      name: "Boyacá",
+      description: "default",
+      color: "default",
+      hover_color: "default",
+      url: "default"
+    },
+    COCAL: {
+      name: "Caldas",
+      description: "☕ Café: 72%\n\n🍌 Plátano: 65%\n\nClick to view full detail.",
+      color: "#10B981",
+      hover_color: "default",
+      url: "default"
+    },
+    COCAQ: {
+      name: "Caquetá",
+      description: "default",
+      color: "default",
+      hover_color: "default",
+      url: "default"
+    },
+    COCAS: {
+      name: "Casanare",
+      description: "default",
+      color: "default",
+      hover_color: "default",
+      url: "default"
+    },
+    COCAU: {
+      name: "Cauca",
+      description: "default",
+      color: "default",
+      hover_color: "default",
+      url: "default"
+    },
+    COCES: {
+      name: "Cesar",
+      description: "default",
+      color: "default",
+      hover_color: "default",
+      url: "default"
+    },
+    COCHO: {
+      name: "Chocó",
+      description: "☕ Café: 72%\n\n🍌 Plátano: 65%\n\nClick to view full detail.",
+      color: "#EF4444",
+      hover_color: "default",
+      url: "default"
+    },
+    COCOR: {
+      name: "Córdoba",
+      description: "default",
+      color: "default",
+      hover_color: "default",
+      url: "default"
+    },
+    COCUN: {
+      name: "Cundinamarca",
+      description: "☕ Café: 72%\n\n🍌 Plátano: 65%\n\nClick to view full detail.",
+      color: "#10B981",
+      hover_color: "default",
+      url: "default"
+    },
+    CODC: {
+      name: "Distrito Capital de Bogotá",
+      description: "default",
+      color: "default",
+      hover_color: "default",
+      url: "default"
+    },
+    COGUA: {
+      name: "Guainía",
+      description: "default",
+      color: "default",
+      hover_color: "default",
+      url: "default"
+    },
+    COGUV: {
+      name: "Guaviare",
+      description: "default",
+      color: "default",
+      hover_color: "default",
+      url: "default"
+    },
+    COHUI: {
+      name: "Huila",
+      description: "default",
+      color: "default",
+      hover_color: "default",
+      url: "default"
+    },
+    COLAG: {
+  name: "La Guajira",
+  description: "☕ Café: 72%\n\n🍌 Plátano: 65%\n\nClick to view full detail.",
+      color: "#EAB308",
+      hover_color: "default",
+      url: "default"
+    },
+    COMAG: {
+      name: "Magdalena",
+      description: "default",
+      color: "default",
+      hover_color: "default",
+      url: "default"
+    },
+    COMET: {
+      name: "Meta",
+      description: "default",
+      color: "default",
+      hover_color: "default",
+      url: "default"
+    },
+    CONAR: {
+      name: "Nariño",
+      description: "default",
+      color: "default",
+      hover_color: "default",
+      url: "default"
+    },
+    CONSA: {
+      name: "Norte de Santander",
+      description: "default",
+      color: "default",
+      hover_color: "default",
+      url: "default"
+    },
+    COPUT: {
+      name: "Putumayo",
+      description: "default",
+      color: "default",
+      hover_color: "default",
+      url: "default"
+    },
+    COQUI: {
+      name: "Quindío",
+      description: "default",
+      color: "default",
+      hover_color: "default",
+      url: "default"
+    },
+    CORIS: {
+      name: "Risaralda",
+      description: "default",
+      color: "default",
+      hover_color: "default",
+      url: "default"
+    },
+    COSAN: {
+      name: "Santander",
+      description: "☕ Café: 72%\n\n🍌 Plátano: 65%\n\nClick to view full detail.",
+      color: "default",
+      hover_color: "default",
+      url: "default"
+    },
+    COSAP: {
+      name: "San Andrés y Providencia",
+      description: "default",
+      color: "default",
+      hover_color: "default",
+      url: "default"
+    },
+    COSUC: {
+      name: "Sucre",
+      description: "default",
+      color: "default",
+      hover_color: "default",
+      url: "default"
+    },
+    COTOL: {
+      name: "Tolima",
+      description: "default",
+      color: "default",
+      hover_color: "default",
+      url: "default"
+    },
+    COVAC: {
+      name: "Valle del Cauca",
+      description: "☕ Café: 72%\n\n🍌 Plátano: 65%\n\nClick to view full detail.",
+      color: "#10B981",
+      hover_color: "default",
+      url: "default"
+    },
+    COVAU: {
+      name: "Vaupés",
+      description: "default",
+      color: "default",
+      hover_color: "default",
+      url: "default"
+    },
+    COVID: {
+      name: "Vichada",
+      description: "default",
+      color: "default",
+      hover_color: "default",
+      url: "default"
+    }
+  },
+  locations: {
+    "0": {
+      name: "Bogotá",
+      lat: "4.649178",
+      lng: "-74.062827"
+    }
+  },
+  labels: {
+    COAMA: {
+      name: "Amazonas",
+      parent_id: "COAMA"
+    },
+    COANT: {
+      name: "Antioquia",
+      parent_id: "COANT"
+    },
+    COARA: {
+      name: "Arauca",
+      parent_id: "COARA"
+    },
+    COATL: {
+      name: "Atlántico",
+      parent_id: "COATL"
+    },
+    COBOL: {
+      name: "Bolívar",
+      parent_id: "COBOL"
+    },
+    COBOY: {
+      name: "Boyacá",
+      parent_id: "COBOY"
+    },
+    COCAL: {
+      name: "Caldas",
+      parent_id: "COCAL"
+    },
+    COCAQ: {
+      name: "Caquetá",
+      parent_id: "COCAQ"
+    },
+    COCAS: {
+      name: "Casanare",
+      parent_id: "COCAS"
+    },
+    COCAU: {
+      name: "Cauca",
+      parent_id: "COCAU"
+    },
+    COCES: {
+      name: "Cesar",
+      parent_id: "COCES"
+    },
+    COCHO: {
+      name: "Chocó",
+      parent_id: "COCHO"
+    },
+    COCOR: {
+      name: "Córdoba",
+      parent_id: "COCOR"
+    },
+    COCUN: {
+      name: "Cundinamarca",
+      parent_id: "COCUN"
+    },
+    CODC: {
+      name: "Distrito Capital de Bogotá",
+      parent_id: "CODC"
+    },
+    COGUA: {
+      name: "Guainía",
+      parent_id: "COGUA"
+    },
+    COGUV: {
+      name: "Guaviare",
+      parent_id: "COGUV"
+    },
+    COHUI: {
+      name: "Huila",
+      parent_id: "COHUI"
+    },
+    COLAG: {
+      name: "La Guajira",
+      parent_id: "COLAG"
+    },
+    COMAG: {
+      name: "Magdalena",
+      parent_id: "COMAG"
+    },
+    COMET: {
+      name: "Meta",
+      parent_id: "COMET"
+    },
+    CONAR: {
+      name: "Nariño",
+      parent_id: "CONAR"
+    },
+    CONSA: {
+      name: "Norte de Santander",
+      parent_id: "CONSA"
+    },
+    COPUT: {
+      name: "Putumayo",
+      parent_id: "COPUT"
+    },
+    COQUI: {
+      name: "Quindío",
+      parent_id: "COQUI"
+    },
+    CORIS: {
+      name: "Risaralda",
+      parent_id: "CORIS"
+    },
+    COSAN: {
+      name: "Santander",
+      parent_id: "COSAN"
+    },
+    COSAP: {
+      name: "San Andrés y Providencia",
+      parent_id: "COSAP"
+    },
+    COSUC: {
+      name: "Sucre",
+      parent_id: "COSUC"
+    },
+    COTOL: {
+      name: "Tolima",
+      parent_id: "COTOL"
+    },
+    COVAC: {
+      name: "Valle del Cauca",
+      parent_id: "COVAC"
+    },
+    COVAU: {
+      name: "Vaupés",
+      parent_id: "COVAU"
+    },
+    COVID: {
+      name: "Vichada",
+      parent_id: "COVID"
+    }
+  }
+};
